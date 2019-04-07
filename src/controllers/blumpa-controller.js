dcaee9c760618588334c91c29d1cf023106de550
mateusgpontes <mateus.gpontes@hotmail.com>
2019-04-07 18:04:39 -0300

    /test-bumpla route returning [1-1000] array

--- src/controllers/blumpa-controller.js
+++ src/controllers/blumpa-controller.js
@@ -2,5 +2,11 @@
 'use strict';
 
 exports.get = async(req, res, next) => {
-    res.status(200).send("ok");
+    let list = [];
+    for(let i = 1; i <= 1000; i++){
+
+        list.push(i)
+
+    }
+    res.status(200).json(list);
 };
