53eea2fd59ade088ecc0e1e99bb1b7a66e2c8100
mateusgpontes <mateus.gpontes@hotmail.com>
2019-04-07 18:26:28 -0300

    added condition for multiples of 3 and 5

--- src/controllers/blumpa-controller.js
+++ src/controllers/blumpa-controller.js
@@ -4,17 +4,23 @@
 exports.get = async(req, res, next) => {
     let list = [];
     for(let i = 1; i <= 1000; i++){
-        let aux = i;
 
-        if(aux % 3 === 0){
-            aux = "fizz";
+        let aux = i;
+        const f = "fizz";
+        const b = "buzz";
+
+        if(aux % 3 === 0 && aux % 5 === 0){
+            aux = f + b;
+        }
+        else if(aux % 3 === 0){
+            aux = f;
 
         }else if(aux % 5 === 0){
-            aux = "buzz"
+            aux = b;
         };
 
-        list.push(aux)
+        list.push(aux);
 
-    }
+    };
     res.status(200).json(list);
 };
